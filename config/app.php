02eb14e07d72554b3372df5fe04048b688cf1c73
Samuel Gómez Balderas <sierra_034@outlook.com>
2019-06-10 04:17:53 -0500

    installed (caffeinated/shinobi)

--- config/app.php
+++ config/app.php
@@ -175,6 +175,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /**
+         * Extra Service Providers...
+         */
+        Caffeinated\Shinobi\ShinobiServiceProvider::class,
+
     ],
 
     /*
