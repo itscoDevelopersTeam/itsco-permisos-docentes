769bac133ade5c2673e2e8609e813c9d905e4f73
Samuel Gómez Balderas <sierra_034@outlook.com>
2019-06-14 07:20:39 -0500

    User and Middleware registered

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,6 +60,11 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+
+        /**
+         * Extra Middlewares
+         */
+        'has.role' => \Caffeinated\Shinobi\Middleware\UserHasRole::class
     ];
 
     /**
